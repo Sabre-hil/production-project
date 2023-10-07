478d94cf4a07af5923768f8ce4fe7b266cf85fec
Sabre-hil <abdeevhizir95@gmail.com>
2023-10-08 00:41:06 +0300

    added ErrorBoundary

--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -3,10 +3,10 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './NotFoundPage.module.scss';
 
 const NotFoundPage = () => {
-  const { t } = useTranslation('not-found');
+  const { t } = useTranslation();
   return (
     <div className={classNames(cls.NotFounPage, {}, [])}>
-      <h2>{t('Такой страницы не существует')}</h2>
+      <h2>{t('Страница не найдена')}</h2>
     </div>
   );
 };
