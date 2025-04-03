2e2525da722bc8bebbec76a5d2f8d51fe68ffe1a
Khizir Abdeev <abdeevhizir95@gmail.com>
2025-04-03 18:31:22 +0300

    add virtualized list

--- src/widgets/Page/Page.tsx
+++ src/widgets/Page/Page.tsx
@@ -22,6 +22,8 @@ interface PageProps {
   onScrollEnd?: () => void;
 }
 
+export const PAGE_ID = 'PAGE_ID';
+
 export const Page = memo((props: PageProps) => {
   const { className, children, onScrollEnd } = props;
   const dispatch = useAppDispatch();
@@ -54,6 +56,7 @@ export const Page = memo((props: PageProps) => {
       ref={wrapperRef}
       className={classNames(cls.Page, {}, [className])}
       onScroll={onScroll}
+      id={PAGE_ID}
     >
       {children}
       {onScrollEnd && <div className={cls.trigger} ref={triggerRef} />}
